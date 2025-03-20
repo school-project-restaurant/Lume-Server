6d77940941c9e37cf37f0a6df6384bab681763b5
Rohozin Maksym <rohozin.max@gmail.com>
2025-03-20 11:41:57 +0100

    Refactor entity classes

--- Lume.Domain/Entities/Staff.cs
+++ Lume.Domain/Entities/Staff.cs
@@ -1,15 +1,15 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace Lume.Domain.Entities;
 
-public class Customer : IdentityUser<Guid>
+public class Staff : IdentityUser<Guid>
 {
     public override Guid Id { get; set; }
     public string Name { get; set; } = null!;
     public string Surname { get; set; } = null!;
-    public override string? Email { get; set; }
     public override string PhoneNumber { get; set; } = null!;
-    public List<Guid>? ReservationsId { get; set; }
     public override string PasswordHash { get; set; } = null!;
+    public int Salary { get; set; }
+    public bool IsActive { get; set; }
+    public int MonthHours { get; set; }
 }
--- Lume.Domain/Entities/Table.cs
+++ Lume.Domain/Entities/Table.cs
@@ -2,7 +2,7 @@ namespace Lume.Domain.Entities;
 
 public class Table
 {
-    public Guid Number { get; set; }
+    public int Number { get; set; }
     public List<int>? ReservationsId { get; set; }
     public int Seats { get; set; }
 }

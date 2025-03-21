cb455e6f73339dfdaa42ab0ad0f774297b5be732
Rohozin Maksym <rohozin.max@gmail.com>
2025-03-21 09:10:48 +0100

    Remove unused customer service methods

--- Lume.Application/Customers/ICustomerService.cs
+++ Lume.Application/Customers/ICustomerService.cs
@@ -6,11 +6,6 @@ namespace Lume.Application.Customers;
 
 public interface ICustomerService
 {
-    public Task<IEnumerable<CustomerDto>> GetAll();
-    Task<CustomerDto?> GetById(int id);
-    Task Create(Customer customer);
-    Task Update(int id, CustomerDto customer);
-    Task Delete(int id);
     Task<ReservationDto> GetReservations(int id);
     Task CreateReservation(int id, Reservation reservation);
 }

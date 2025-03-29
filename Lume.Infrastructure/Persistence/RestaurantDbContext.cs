30b55a6a81f7628990b82ee6b8ecc4a2be3e990f
Rohozin Maksym <rohozin.max@gmail.com>
2025-03-29 17:37:06 +0100

    Refactor RestaurantDbContext to inherit from IdentityDbContext. Also implement TPH pattern with ApplicationUser

--- Lume.Infrastructure/Persistence/RestaurantDbContext.cs
+++ Lume.Infrastructure/Persistence/RestaurantDbContext.cs
@@ -1,24 +1,28 @@
 using Lume.Domain.Entities;
-        using Microsoft.AspNetCore.Identity;
-        using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-        using Microsoft.EntityFrameworkCore;
-        
-        namespace Lume.Infrastructure.Persistence;
-        
-        internal class RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) : DbContext(options)
-        {
-            public DbSet<Customer> Customers { get; set; }
-            public DbSet<Reservation> Reservations { get; set; }
-            public DbSet<Table> Tables { get; set; }
-            public DbSet<Staff> Staffs { get; set; }
-        
-            protected override void OnModelCreating(ModelBuilder modelBuilder)
-            {
-                base.OnModelCreating(modelBuilder);
-        
-                modelBuilder.Entity<Table>().HasKey(t => t.Number);
-                modelBuilder.Entity<Customer>().HasKey(c => c.PhoneNumber);
-                modelBuilder.Entity<Reservation>().HasKey(r => r.Id);
-                modelBuilder.Entity<Staff>().HasKey(s => s.Id);
-            }
-        }
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lume.Infrastructure.Persistence;
+
+internal class RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) 
+    : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>(options)
+{
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Table> Tables { get; set; }
+    public DbSet<Staff> Staffs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<ApplicationUser>()
+            .HasDiscriminator<string>("UserType")
+            .HasValue<Customer>("Customer")
+            .HasValue<Staff>("Staff");
+
+        modelBuilder.Entity<Table>().HasKey(t => t.Number);
+        modelBuilder.Entity<Reservation>().HasKey(r => r.Id);
+    }
+}

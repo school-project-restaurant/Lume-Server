22a57c551e5b730c5c33836024f4922cae40ed72
Rohozin Maksym <rohozin.max@gmail.com>
2025-03-29 17:23:05 +0100

    Refactor Staff class to inherit from ApplicationUser

--- Lume.Domain/Entities/Staff.cs
+++ Lume.Domain/Entities/Staff.cs
@@ -1,14 +1,7 @@
-using Microsoft.AspNetCore.Identity;
-
 namespace Lume.Domain.Entities;
 
-public class Staff : IdentityUser<Guid>
+public class Staff : ApplicationUser
 {
-    public override Guid Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string Surname { get; set; } = null!;
-    public override string PhoneNumber { get; set; } = null!;
-    public override string PasswordHash { get; set; } = null!;
     public int Salary { get; set; }
     public bool IsActive { get; set; }
     public int MonthHours { get; set; }

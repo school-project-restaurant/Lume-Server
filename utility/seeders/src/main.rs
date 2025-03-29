9e2da05e05284e3acc1452bbd60a16d0756bca10
Pik4xe <111708124+Pik4xe@users.noreply.github.com>
2025-03-29 05:43:44 +0000

    Update main.rs, added requested changes

--- utility/seeders/src/main.rs
+++ utility/seeders/src/main.rs
@@ -1,3 +1,4 @@
+use chrono::{Duration, Utc};
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
@@ -10,8 +11,8 @@ struct Customer {
     name: String,
     surname: String,
     email: String,
-    phone: String,
-    reservations_id: Vec<String>,
+    phoneNumber: String,
+    reservationsId: Vec<String>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -19,29 +20,44 @@ struct Staff {
     id: String,
     name: String,
     surname: String,
-    phone: String,
-    password_hash: String,
+    phoneNumber: String,
+    passwordHash: String,
     salary: i32,
-    is_active: bool,
-    month_hours: i32,
+    isActive: bool,
+    monthHours: i32,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Table {
     number: i32,
-    reservations_id: Vec<String>,
+    reservationsId: Vec<String>,
     seats: i32,
 }
 
 #[derive(Serialize, Deserialize)]
 struct Reservation {
     id: String,
-    customer_id: String,
+    customerId: String,
     date: String,
-    table: Vec<i32>,
-    guest_count: i32,
+    tableNumber: Vec<i32>,
+    guestCount: i32,
     status: String,
     notes: String,
+}
+
+fn randomDate() -> String {
+    // Get today's date
+    let today = Utc::now().date_naive();
+
+    // Generate a random number of days to add to today's date
+    let mut rng = rand::thread_rng();
+    let random_days = rng.gen_range(0..=730); // 0 to 730 days (2 years)
+
+    // Calculate the random date
+    let random_date = today + Duration::days(random_days);
+
+    // Format the date in the c# entity framework format
+    random_date.format("%Y-%m-%dT%H:%M:%S").to_string()
 }
 
 fn generate_name() -> String {
@@ -99,7 +115,7 @@ fn main() {
     let mut rng = rand::thread_rng();
 
     // Generate 50 customers
-    for _ in 0..50 {
+    for _ in 0..10 {
         let customer_name = generate_name();
         let customer_surname = generate_name();
         let customer_email = format!("{}{}@example.com", customer_name, customer_surname);
@@ -110,15 +126,15 @@ fn main() {
             name: customer_name,
             surname: customer_surname,
             email: customer_email,
-            phone: customer_phone,
-            reservations_id: Vec::new(),
+            phoneNumber: customer_phone,
+            reservationsId: Vec::new(),
         };
 
         customers.push(customer);
     }
 
     // Generate 20 staff members
-    for _ in 0..20 {
+    for _ in 0..10 {
         let staff_name = generate_name();
         let staff_surname = generate_name();
         let staff_phone = generate_phone_number();
@@ -127,21 +143,21 @@ fn main() {
             id: Uuid::new_v4().to_string(),
             name: staff_name,
             surname: staff_surname,
-            phone: staff_phone,
-            password_hash: generate_password_hash(&random_string()),
-            salary: 50000,
-            is_active: true,
-            month_hours: 160,
+            phoneNumber: staff_phone,
+            passwordHash: generate_password_hash(&random_string()),
+            salary: (1000 + 10 * rng.gen_range(1..70)),
+            isActive: rng.gen(),
+            monthHours: (100 + 10 * rng.gen_range(1..7)),
         };
 
         staff_members.push(staff);
     }
 
     // Generate 10 tables
-    for i in 1..=10 {
+    for i in 1..=15 {
         let table = Table {
             number: i,
-            reservations_id: Vec::new(),
+            reservationsId: Vec::new(),
             seats: rng.gen_range(2..=10), // Random number of seats between 2 and 10
         };
 
@@ -149,28 +165,33 @@ fn main() {
     }
 
     // Generate 100 reservations
-    for _ in 0..100 {
+    for _ in 0..30 {
         // Randomly pick a customer's index and a table index
         let customer_index = rng.gen_range(0..customers.len());
         let table_index = rng.gen_range(0..tables.len());
 
+        let possibleStatus: Vec<String> = vec![
+            "confirmed".to_string(),
+            "pending".to_string(),
+            "rejected".to_string(),
+        ];
         let reservation_id = Uuid::new_v4().to_string();
         let reservation = Reservation {
             id: reservation_id.clone(),
-            customer_id: customers[customer_index].id.clone(),
-            date: "2023-10-01T19:00:00".to_string(),
-            table: vec![tables[table_index].number],
-            guest_count: 2,
-            status: "confirmed".to_string(),
+            customerId: customers[customer_index].id.clone(),
+            date: randomDate(),
+            tableNumber: vec![tables[table_index].number],
+            guestCount: rng.gen_range(1..15),
+            status: possibleStatus[rng.gen_range(0..3)].clone(),
             notes: "No special requests".to_string(),
         };
 
         // Add the reservation id to the customer's reservations_id list
         customers[customer_index]
-            .reservations_id
+            .reservationsId
             .push(reservation_id.clone());
         // Add the reservation id to the table's reservations_id list
-        tables[table_index].reservations_id.push(reservation_id);
+        tables[table_index].reservationsId.push(reservation_id);
 
         reservations.push(reservation);
     }

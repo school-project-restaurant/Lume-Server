6f937e8348e960280ab655fecc9ec1c70f6a387d
Rohozin Maksym <rohozin.max@gmail.com>
2025-04-01 22:11:11 +0200

    Refactor RestaurantDbContext to remove Customer and Staff DbSets; simplify ApplicationUser configuration

--- Lume.Infrastructure/Persistence/RestaurantDbContext.cs
+++ Lume.Infrastructure/Persistence/RestaurantDbContext.cs
@@ -8,20 +8,16 @@ namespace Lume.Infrastructure.Persistence;
 internal class RestaurantDbContext(DbContextOptions<RestaurantDbContext> options) 
     : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>(options)
 {
-    public DbSet<Customer> Customers { get; set; }
     public DbSet<Reservation> Reservations { get; set; }
     public DbSet<Table> Tables { get; set; }
-    public DbSet<Staff> Staffs { get; set; }
+    
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<ApplicationUser>()
-            .HasDiscriminator<string>("UserType")
-            .HasValue<Customer>("Customer")
-            .HasValue<Staff>("Staff");
-
+        modelBuilder.Entity<ApplicationUser>();
+        
         modelBuilder.Entity<Table>().HasKey(t => t.Number);
         modelBuilder.Entity<Reservation>().HasKey(r => r.Id);
     }

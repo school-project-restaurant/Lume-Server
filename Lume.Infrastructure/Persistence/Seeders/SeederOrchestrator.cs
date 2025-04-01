51ffcfea4918cf19a66c8cda407d3dc24fb44066
Rohozin Maksym <rohozin.max@gmail.com>
2025-04-01 22:13:05 +0200

    Make SeederOrchestrator class internal for encapsulation

--- Lume.Infrastructure/Persistence/Seeders/SeederOrchestrator.cs
+++ Lume.Infrastructure/Persistence/Seeders/SeederOrchestrator.cs
@@ -1,6 +1,6 @@
 namespace Lume.Infrastructure.Persistence.Seeders;
 
-public class SeederOrchestrator(IEnumerable<ISeeder> seeders) : ISeederOrchestrator
+internal class SeederOrchestrator(IEnumerable<ISeeder> seeders) : ISeederOrchestrator
 {
     public async Task SeedAllAsync()
     {

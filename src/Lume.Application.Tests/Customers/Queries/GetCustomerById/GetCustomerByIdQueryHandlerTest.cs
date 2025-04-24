230872f4fd5269b40fe5b987b7a96cbaf6cb9846
Rohozin Maksym <rohozin.max@gmail.com>
2025-04-24 12:37:07 +0200

    feat: implement test for GetCustomerByIdQueryHandler to validate customer retrieval

--- src/Lume.Application.Tests/Customers/Queries/GetCustomerById/GetCustomerByIdQueryHandlerTest.cs
+++ src/Lume.Application.Tests/Customers/Queries/GetCustomerById/GetCustomerByIdQueryHandlerTest.cs
@@ -1,5 +1,15 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
 using JetBrains.Annotations;
+using Lume.Application.Customers.Dtos;
 using Lume.Application.Customers.Queries.GetCustomerById;
+using Lume.Domain.Entities;
+using Lume.Domain.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 
 namespace Lume.Application.Tests.Customers.Queries.GetCustomerById;
@@ -9,8 +19,36 @@ public class GetCustomerByIdQueryHandlerTest
 {
 
     [Fact]
-    public void METHOD()
+    public async Task Handle_WhenIdIsValid_ReturnCustomerDto()
     {
+        // arrange
+        var loggerMock = new Mock<ILogger<GetCustomerByIdQueryHandler>>();
+        var mapperMock = new Mock<IMapper>();
+        var customerRepositoryMock = new Mock<ICustomerRepository>();
         
+        Guid customerId = Guid.NewGuid(); 
+        var query = new GetCustomerByIdQuery(customerId);
+
+        var customer = new ApplicationUser
+        {
+            Id = customerId,
+            UserType = "Customer"
+        };
+        var expectedCustomerDto = new CustomerDto
+        {
+            Id = customerId,
+        };
+        
+        mapperMock.Setup(m => m.Map<CustomerDto>(customer)).Returns(expectedCustomerDto);
+        customerRepositoryMock.Setup(r => r.GetCustomerById(customerId)).ReturnsAsync(customer);
+        
+        var handler = new GetCustomerByIdQueryHandler(loggerMock.Object, mapperMock.Object, customerRepositoryMock.Object);
+        
+        // act
+        var result = await handler.Handle(query, CancellationToken.None);
+        
+        // assert
+        result.Should().BeEquivalentTo(expectedCustomerDto);
+        customerRepositoryMock.Verify(r => r.GetCustomerById(customerId), Times.Once);
     }
 }

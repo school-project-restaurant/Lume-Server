984b9bc3b066618f69b6dbf6063469bcd5340857
Rohozin Maksym <rohozin.max@gmail.com>
2025-04-24 21:50:02 +0200

    fix: remove null assigning

--- Lume.Application/Staff/Commands/CreateStaff/CreateStaffCommand.cs
+++ Lume.Application/Staff/Commands/CreateStaff/CreateStaffCommand.cs
@@ -6,7 +6,7 @@ public class CreateStaffCommand : IRequest<Guid>
 {
     public string Name { get; set; } = null!;
     public string Surname { get; set; } = null!;
-    public double Salary { get; set; } = null!;
+    public double Salary { get; set; }
     public string PhoneNumber { get; set; } = null!;
     public string PasswordHash { get; set; } = null!;
 }

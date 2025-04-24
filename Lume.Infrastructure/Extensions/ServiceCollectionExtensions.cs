02b0a93927865e3b1a131657c06f0bfd4dd8dfbf
Rohozin Maksym <rohozin.max@gmail.com>
2025-04-24 22:13:49 +0200

    feat: register IStaffRepository in ServiceCollectionExtensions for dependency injection

--- Lume.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Lume.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -32,6 +32,7 @@ public static class ServiceCollectionExtensions
         services.AddDbContext<RestaurantDbContext>(options =>
             options.UseNpgsql(connectionString));
         services.AddScoped<ICustomerRepository, CustomerRepository>();
+        services.AddScoped<IStaffRepository, StaffRepository>();
         
         services.AddTransient<ISeeder, UserSeeder>();
         services.AddTransient<ISeeder, TableSeeder>();

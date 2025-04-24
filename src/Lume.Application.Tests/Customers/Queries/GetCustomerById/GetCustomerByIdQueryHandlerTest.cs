0ddd2e26ce7f9a9d147787dd708cc754afe5494e
Rohozin Maksym <rohozin.max@gmail.com>
2025-04-24 12:26:58 +0200

    feat: add initial test for GetCustomerByIdQueryHandler.cs

---
+++ src/Lume.Application.Tests/Customers/Queries/GetCustomerById/GetCustomerByIdQueryHandlerTest.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+using Lume.Application.Customers.Queries.GetCustomerById;
+using Xunit;
+
+namespace Lume.Application.Tests.Customers.Queries.GetCustomerById;
+
+[TestSubject(typeof(GetCustomerByIdQueryHandler))]
+public class GetCustomerByIdQueryHandlerTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}

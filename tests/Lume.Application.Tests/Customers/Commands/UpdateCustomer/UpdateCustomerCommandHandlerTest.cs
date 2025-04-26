019d65558789b18b2a135e9ca301cd7633136662
Rohozin Maksym <rohozin.max@gmail.com>
2025-04-26 18:32:17 +0200

    feat: add unit tests for UpdateCustomerCommandHandler

---
+++ tests/Lume.Application.Tests/Customers/Commands/UpdateCustomer/UpdateCustomerCommandHandlerTest.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using FluentAssertions;
+using JetBrains.Annotations;
+using Lume.Application.Customers.Commands.UpdateCustomer;
+using Lume.Domain.Entities;
+using Lume.Domain.Repositories;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Lume.Application.Tests.Customers.Commands.UpdateCustomer;
+
+[TestSubject(typeof(UpdateCustomerCommandHandler))]
+public class UpdateCustomerCommandHandlerTest
+{
+
+    [Fact]
+    public async Task Handle_WhenRequestIsValid_ShouldReturnTrue()
+    {
+        // arrange
+        var loggerMock = new Mock<ILogger<UpdateCustomerCommandHandler>>();
+        var mapperMock = new Mock<IMapper>();
+        var customerRepositoryMock = new Mock<ICustomerRepository>();
+        
+        Guid customerId = Guid.NewGuid();
+        var request = new UpdateCustomerCommand
+        {
+            Id = customerId,
+            Email = "rand@example.com",
+            Name = "Random",
+            Surname = "Random",
+            PhoneNumber = "+1234567890122"
+        };
+
+        var customer = new ApplicationUser
+        {
+            Id = customerId,
+            Email = "random@example.com",
+            Name = "Random Name",
+            Surname = "Random Surname",
+            PhoneNumber = "+1234567890123",
+            UserType = "Customer"
+        };
+        customerRepositoryMock.Setup(r => r.GetCustomerById(customerId)).ReturnsAsync(customer);
+        mapperMock.Setup(m => m.Map(request, customer)).Callback<UpdateCustomerCommand, ApplicationUser>
+            ((src, dest) =>
+            {
+                dest.Email = src.Email;
+                dest.Name = src.Name;
+                dest.Surname = src.Surname;
+                dest.PhoneNumber = src.PhoneNumber;
+            });
+        
+        var handler =
+            new UpdateCustomerCommandHandler(loggerMock.Object, mapperMock.Object, customerRepositoryMock.Object);
+        
+        // act
+        var result = await handler.Handle(request, CancellationToken.None);
+        
+        // assert
+        result.Should().Be(true);
+        mapperMock.Verify(m => m.Map(request, customer), Times.Once);
+        customerRepositoryMock.Verify(r => r.SaveChanges(), Times.Once);
+        
+        customer.Email.Should().Be(request.Email);
+        customer.Name.Should().Be(request.Name);
+        customer.Surname.Should().Be(request.Surname);
+        customer.PhoneNumber.Should().Be(request.PhoneNumber);
+    }
+}

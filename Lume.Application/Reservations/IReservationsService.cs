83955fe3a6a021f814d379b34e110eb0ff84bf5b
Rohozin Maksym <rohozin.max@gmail.com>
2025-04-26 19:02:13 +0200

    fix: delete unnecessary services

--- Lume.Application/Reservations/IReservationsService.cs
+++
@@ -1,9 +0,0 @@
-using Lume.Application.Reservations.Dtos;
-
-namespace Lume.Application.Reservations;
-
-public interface IReservationService
-{
-    Task<ReservationDto> GetReservationById(Guid id);
-    Task<Guid> CreateReservation(Guid Id, int Seats); // ???
-}

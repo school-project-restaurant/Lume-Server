ca88de6899ba5f51e584301ebea830cae9f89a0c
Pik4xe <ericsemprini99@gmail.com>
2025-04-26 02:24:08 +0200

    Feat: get reservation by id

---
+++ Lume.Application/Reservations/Queries/GetReservationById/GetReservationByIdQuery.cs
@@ -0,0 +1,9 @@
+using Lume.Application.Reservations.Dtos;
+using MediatR;
+
+namespace Lume.Application.Reservations.Queries.GetReservationById;
+
+public class GetReservationByIdQuery(Guid id) : IRequest<ReservationDto?>
+{
+    public Guid Id { get; set; } = id;
+}

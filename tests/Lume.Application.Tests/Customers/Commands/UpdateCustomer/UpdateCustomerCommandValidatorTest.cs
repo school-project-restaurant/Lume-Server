c06786c4079139d0178e18934c25d451ddbc2119
Rohozin Maksym <rohozin.max@gmail.com>
2025-04-26 20:11:54 +0200

    feat: add boundary value tests for UpdateCustomerCommandValidator

--- tests/Lume.Application.Tests/Customers/Commands/UpdateCustomer/UpdateCustomerCommandValidatorTest.cs
+++ tests/Lume.Application.Tests/Customers/Commands/UpdateCustomer/UpdateCustomerCommandValidatorTest.cs
@@ -58,4 +58,27 @@ public class UpdateCustomerCommandValidatorTest
         result.ShouldHaveValidationErrorFor(nameof(command.Email));
         result.ShouldHaveValidationErrorFor(nameof(command.PhoneNumber));
     }
+    
+    [Fact]
+    public void Validator_ForBoundaryValues_ShouldNotHaveValidationErrors()
+    {
+        // arrange
+        var command = new UpdateCustomerCommand()
+        {
+            Name = new string('A', 50),
+            Surname = new string('B', 50),
+            Email = "a@b.com",
+            PhoneNumber = "+1234567890123"
+        };
+        var validator = new UpdateCustomerCommandValidator();
+
+        // act
+        var result = validator.TestValidate(command);
+
+        // assert
+        result.ShouldNotHaveValidationErrorFor(nameof(command.Name));
+        result.ShouldNotHaveValidationErrorFor(nameof(command.Surname));
+        result.ShouldNotHaveValidationErrorFor(nameof(command.Email));
+        result.ShouldNotHaveValidationErrorFor(nameof(command.PhoneNumber));
+    }
 }

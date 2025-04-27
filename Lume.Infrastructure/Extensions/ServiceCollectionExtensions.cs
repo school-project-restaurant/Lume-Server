946ac1fc6c5e4db6d9dd8d6155b6dab4750db667
Pik4xe <ericsemprini99@gmail.com>
2025-04-27 02:37:18 +0200

    refractor: added Plate repository and IPlateRepository to the services

--- Lume.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Lume.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -34,6 +34,7 @@ public static class ServiceCollectionExtensions
             options.UseNpgsql(connectionString));
         services.AddScoped<ICustomerRepository, CustomerRepository>();
         services.AddScoped<IReservationRepository, ReservationRepository>();
+        services.AddScoped<IPlateRepository, PlateRepository>();
         services.AddScoped<ITablesRepository, TableRepository>();
         services.AddScoped<IStaffRepository, StaffRepository>();
 

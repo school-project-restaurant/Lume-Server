8810309d00c6864d2762855f832b57b244483971
Rohozin Maksym <rohozin.max@gmail.com>
2025-04-30 14:37:40 +0200

    test: add unit tests for GetCustomerById method in CustomersController

--- tests/Lume.API.Tests/Controllers/CustomersControllerTest.cs
+++ tests/Lume.API.Tests/Controllers/CustomersControllerTest.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Net;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 using FluentAssertions;
 using JetBrains.Annotations;
+using Lume.Application.Customers.Commands.CreateCustomer;
 using Lume.Application.Customers.Dtos;
 using Lume.Controllers;
+using Lume.Domain.Entities;
 using Lume.Domain.Repositories;
 using Lume.Infrastructure.Persistence.Seeders;
+using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +33,7 @@ public class CustomersControllerTest : IClassFixture<WebApplicationFactory<Progr
         {
             builder.ConfigureTestServices(services =>
             {
+                services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
                 services.Replace(ServiceDescriptor.Scoped(typeof(ICustomerRepository),
                     _ => _customerRepositoryMock.Object));
                 services.Replace(ServiceDescriptor.Singleton(typeof(ISeederOrchestrator),
@@ -49,4 +54,45 @@ public class CustomersControllerTest : IClassFixture<WebApplicationFactory<Progr
         result.StatusCode.Should().Be(HttpStatusCode.OK);
     }
     
+    [Fact]
+    public async Task GetCustomerById_WhenRequestIsValid_Returns200Ok()
+    {
+        // arrange
+        var client = _factory.CreateClient();
+        var id = Guid.NewGuid();
+        var customer = new ApplicationUser
+        {
+            Id = id,
+            Name = "Test Name",
+            Surname = "Test Surname",
+            UserType = "Customer"
+        };
+        _customerRepositoryMock.Setup(r => r.GetCustomerById(id)).ReturnsAsync(customer);
+        
+        // act
+        var result = await client.GetAsync($"/api/Customers/{id}");
+        var customerDto = await result.Content.ReadFromJsonAsync<CustomerDto>();
+        
+        // assert
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+        customerDto.Should().NotBeNull();
+        customerDto.Name.Should().Be("Test Name");
+        customerDto.Surname.Should().Be("Test Surname");
+    }
+    
+    [Fact]
+    public async Task GetCustomerById_WhenCustomerDoesNotExist_Returns404NotFound()
+    {
+        // arrange
+        var client = _factory.CreateClient();
+        var id = Guid.NewGuid();
+        _customerRepositoryMock.Setup(r => r.GetCustomerById(id)).ReturnsAsync((ApplicationUser)null);
+        
+        // act
+        var result = await client.GetAsync($"/api/Customers/{id}");
+        
+        // assert
+        result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+
 }

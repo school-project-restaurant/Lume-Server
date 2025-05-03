a6f766ec3939344f162af62fd823a274ced5764c
Rohozin Maksym <rohozin.max@gmail.com>
2025-05-03 09:23:07 +0200

    feat: add CustomerFilterOptions and CustomerSortOptions classes to support filtering and sorting of customers

--- src/Lume.Domain/Repositories/ICustomerRepository.cs
+++ src/Lume.Domain/Repositories/ICustomerRepository.cs
@@ -1,3 +1,4 @@
+using Lume.Domain.Constants;
 using Lume.Domain.Entities;
 
 namespace Lume.Domain.Repositories;
@@ -5,8 +6,27 @@ namespace Lume.Domain.Repositories;
 public interface ICustomerRepository
 {
     Task<IEnumerable<ApplicationUser>> GetAllCustomers();
+    Task<(IEnumerable<ApplicationUser>, int)> GetMatchingCustomers(CustomerFilterOptions filterOptions);
     Task<ApplicationUser?> GetCustomerById(Guid id);
     Task<Guid> CreateCustomer(ApplicationUser customer);
     Task DeleteCustomer(ApplicationUser customer);
     Task SaveChanges();
+}
+
+public class CustomerFilterOptions
+{
+    public string? SearchEmail { get; set; }
+    public string? SearchName { get; set; }
+    public string? SearchSurname { get; set; }
+    public Guid? ReservationId { get; set; }
+    public string? SearchPhone { get; set; }
+
+    public int PageSize { get; set; }
+    public int PageIndex { get; set; }
+}
+
+public class CustomerSortOptions
+{
+    public string? SortBy { get; set; }
+    public SortDirection SortDirection { get; set; }
 }

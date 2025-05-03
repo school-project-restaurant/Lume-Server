4ac5ca07d056de44a582a88c0228e5b884f3139a
Rohozin Maksym <rohozin.max@gmail.com>
2025-05-03 09:23:22 +0200

    feat: update GetAllCustomersQueryHandler to return paged results for customers

--- src/Lume.Application/Customers/Queries/GetAllCustomers/GetAllCustomersQueryHandler.cs
+++ src/Lume.Application/Customers/Queries/GetAllCustomers/GetAllCustomersQueryHandler.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using Lume.Application.Common;
 using Lume.Application.Customers.Dtos;
 using Lume.Domain.Repositories;
 using MediatR;
@@ -7,14 +8,15 @@ using Microsoft.Extensions.Logging;
 namespace Lume.Application.Customers.Queries.GetAllCustomers;
 
 public class GetAllCustomersQueryHandler(ILogger<GetAllCustomersQueryHandler> logger, IMapper mapper,
-    ICustomerRepository customerRepository) : IRequestHandler<GetAllCustomersQuery, IEnumerable<CustomerDto>>
+    ICustomerRepository customerRepository) : IRequestHandler<GetAllCustomersQuery, PagedResult<CustomerDto>>
 {
-    public async Task<IEnumerable<CustomerDto>> Handle(GetAllCustomersQuery request, CancellationToken cancellationToken)
+    public async Task<PagedResult<CustomerDto>> Handle(GetAllCustomersQuery request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Getting all customers from repository {OperationName}", nameof(customerRepository.GetAllCustomers));
         var customers = await customerRepository.GetAllCustomers();
         
         var customersDtos = mapper.Map<IEnumerable<CustomerDto>>(customers);
+        var result = new PagedResult<CustomerDto>(customersDtos, 5, request.PageSize, request.PageIndex);
         return customersDtos!;
     }
 }

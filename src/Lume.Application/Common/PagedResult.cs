85d0df2ec93904338764fd7d416473afed958f84
Rohozin Maksym <rohozin.max@gmail.com>
2025-05-03 09:20:33 +0200

    feat: enhance PagedResult class to include constructor for pagination details

--- src/Lume.Application/Common/PagedResult.cs
+++ src/Lume.Application/Common/PagedResult.cs
@@ -3,10 +3,17 @@ namespace Lume.Application.Common;
 public class PagedResult<T>
 {
     private IEnumerable<T> Items { get; set; }
-    private int PageSize { get; set; }
-    private int PageIndex { get; set; }
     private int TotalPages { get; set; }
     private int TotalItemsCount { get; set; }
     public int ItemsFrom { get; set; }
     public int ItemsTo { get; set; }
+    
+    public PagedResult(IEnumerable<T> items, int totalItemsCount, int pageSize, int pageIndex)
+    {
+        Items = items;
+        TotalItemsCount = totalItemsCount;
+        TotalPages = (int)Math.Ceiling(totalItemsCount / (double)pageSize);
+        ItemsFrom = pageSize * (pageIndex - 1) + 1;
+        ItemsTo = ItemsFrom + pageSize - 1;
+    }
 }

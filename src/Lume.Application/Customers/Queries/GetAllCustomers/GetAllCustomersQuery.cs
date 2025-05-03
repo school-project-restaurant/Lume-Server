6c4d567c3d7b0ab0b5029744f38d5e82d9970628
Rohozin Maksym <rohozin.max@gmail.com>
2025-05-03 09:22:32 +0200

    feat: update GetAllCustomersQuery to support pagination and additional search parameters

--- src/Lume.Application/Customers/Queries/GetAllCustomers/GetAllCustomersQuery.cs
+++ src/Lume.Application/Customers/Queries/GetAllCustomers/GetAllCustomersQuery.cs
@@ -1,9 +1,18 @@
+using Lume.Application.Common;
 using Lume.Application.Customers.Dtos;
+using Lume.Application.Reservations.Dtos;
 using MediatR;
 
 namespace Lume.Application.Customers.Queries.GetAllCustomers;
 
-public class GetAllCustomersQuery : IRequest<IEnumerable<CustomerDto>>
+public class GetAllCustomersQuery : IRequest<PagedResult<CustomerDto>>
 {
-    
+    public string? SearchEmail { get; set; }
+    public string? SearchName { get; set; }
+    public string? SearchSurname { get; set; }
+    public ReservationFindCustomerRequestModel? SearchReservation { get; set; }
+    public string? SearchPhone { get; set; }
+
+    public int PageSize { get; set; }
+    public int PageIndex { get; set; }
 }

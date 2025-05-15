e2f18dc9de675148215fd478ed86d835ef2fec4c
Rohozin Maksym <rohozin.max@gmail.com>
2025-05-15 17:18:14 +0200

    refactor: throw NotFoundException when an entity is not found, in GetBy* handlers

--- src/Lume.Application/Reservations/Queries/GetReservationById/GetReservationByIdQueryHandler.cs
+++ src/Lume.Application/Reservations/Queries/GetReservationById/GetReservationByIdQueryHandler.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Lume.Application.Reservations.Dtos;
+using Lume.Domain.Exceptions;
 using Lume.Domain.Repositories;
 using MediatR;
 using Microsoft.Extensions.Logging;
@@ -7,12 +8,14 @@ using Microsoft.Extensions.Logging;
 namespace Lume.Application.Reservations.Queries.GetReservationById;
 
 public class GetReservationByIdQueryHandler(ILogger<GetReservationByIdQueryHandler> logger, IMapper mapper,
-    IReservationRepository reservationRepository) : IRequestHandler<GetReservationByIdQuery, ReservationDto?>
+    IReservationRepository reservationRepository) : IRequestHandler<GetReservationByIdQuery, ReservationDto>
 {
-    public async Task<ReservationDto?> Handle(GetReservationByIdQuery request, CancellationToken cancellationToken)
+    public async Task<ReservationDto> Handle(GetReservationByIdQuery request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Getting reservation with id {@Id}", request.Id);
         var reservation = await reservationRepository.GetReservationById(request.Id);
+        if (reservation is null)
+            throw new NotFoundException(nameof(reservation), request.Id);
 
         var reservationsDto = mapper.Map<ReservationDto>(reservation);
         return reservationsDto;

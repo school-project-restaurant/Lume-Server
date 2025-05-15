ab62cbb7ef889c2c6fee9626dd90d9f84dabce3a
Rohozin Maksym <rohozin.max@gmail.com>
2025-05-15 12:02:58 +0200

    feat: implement filtering and sorting for GetMatchingDishes in DishRepository

--- src/Lume.Domain/Repositories/IDishRepository.cs
+++ src/Lume.Domain/Repositories/IDishRepository.cs
@@ -1,16 +1,32 @@
+using Lume.Domain.Constants;
 using Lume.Domain.Entities;
 
 namespace Lume.Domain.Repositories;
 
 /// <summary>
-/// Public repository for managing prenotations
+/// Public repository for managing dishes
 /// </summary>
 public interface IDishRepository
 {
-
     Task<IEnumerable<Dish>> GetAllDishes();
+    Task<(IEnumerable<Dish>, int)> GetMatchingDishes(DishFilterOptions filterOptions, DishSortOptions sortOptions);
     Task<Dish?> GetDishById(Guid id);
     Task<Guid> CreateDish(Dish dish);
     Task DeleteDish(Dish dish);
     Task SaveChanges();
+}
+
+public class DishFilterOptions
+{
+    public decimal? MinPrice { get; set; }
+    public decimal? MaxPrice { get; set; }
+    public List<string>? Ingredients { get; set; }
+    public int PageSize { get; set; }
+    public int PageIndex { get; set; }
+}
+
+public class DishSortOptions
+{
+    public string? SortBy { get; set; }
+    public SortDirection? SortDirection { get; set; }
 }
--- src/Lume.Infrastructure/Persistence/Repositories/DishRepository.cs
+++ src/Lume.Infrastructure/Persistence/Repositories/DishRepository.cs
@@ -1,3 +1,4 @@
+using Lume.Domain.Constants;
 using Lume.Domain.Entities;
 using Lume.Domain.Repositories;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,50 @@ internal class DishRepository(RestaurantDbContext dbContext) : IDishRepository
     public async Task<IEnumerable<Dish>> GetAllDishes()
     {
         return await dbContext.Dishes.ToListAsync();
+    }
+
+    public async Task<(IEnumerable<Dish>, int)> GetMatchingDishes(DishFilterOptions filterOptions, DishSortOptions sortOptions)
+    {
+        var query = dbContext.Dishes.AsQueryable();
+        query = ApplySearchFilters(query, filterOptions);
+        query = sortOptions.SortBy is not null 
+            ? ApplySorting(query, sortOptions.SortBy, sortOptions.SortDirection) 
+            : query.OrderBy(d => d.Id);
+
+        int totalCount = await query.CountAsync();
+        var dishes = await query
+            .Skip(filterOptions.PageSize * (filterOptions.PageIndex - 1))
+            .Take(filterOptions.PageSize)
+            .ToListAsync();
+
+        return (dishes, totalCount);
+    }
+
+    private IQueryable<Dish> ApplySearchFilters(IQueryable<Dish> query, DishFilterOptions options)
+    {
+        if (options.MinPrice != null)
+            query = query.Where(d => d.Price >= options.MinPrice);
+    
+        if (options.MaxPrice != null)
+            query = query.Where(d => d.Price <= options.MaxPrice);
+        
+        if (options.Ingredients != null)
+            query = query.Where(d => d.Ingredients == options.Ingredients);
+    
+        return query;
+    }
+    
+    private IQueryable<Dish> ApplySorting(IQueryable<Dish> query, string sortBy, SortDirection? direction)
+    {
+        bool isDescending = direction == SortDirection.Descending;
+    
+        return sortBy.ToLower() switch
+        {
+            "id" => isDescending ? query.OrderByDescending(d => d.Id) : query.OrderBy(d => d.Id),
+            "ingredients" => isDescending ? query.OrderByDescending(d => d.Ingredients) : query.OrderBy(d => d.Ingredients),
+            "price" => isDescending ? query.OrderByDescending(d => d.Price) : query.OrderBy(d => d.Price),
+            _ => throw new KeyNotFoundException($"Property {sortBy} not found or not sortable")
+        };
     }
 
     public async Task<Dish?> GetDishById(Guid id)

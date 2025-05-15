2bd7cde92dcf9525521cede62df69f75c15906a6
Rohozin Maksym <rohozin.max@gmail.com>
2025-05-15 12:49:40 +0200

    refactor: extract service registration logic into AddPresentation extension method

--- src/Lume.API/Program.cs
+++ src/Lume.API/Program.cs
@@ -1,5 +1,6 @@
 using Lume.Application.Extensions;
 using Lume.Domain.Entities;
+using Lume.Extensions;
 using Lume.Infrastructure.Extensions;
 using Microsoft.OpenApi.Models;
 using Lume.Infrastructure.Persistence.Seeders;
@@ -8,50 +9,9 @@ using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-builder.Services.AddAuthentication();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme()
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-    {
-        {
-            new OpenApiSecurityScheme()
-            {
-                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearerAuth" }
-            },
-            []
-        }
-    });
-});
-
-builder.Host.UseSerilog((context, configuration) =>
-{
-    configuration.ReadFrom.Configuration(context.Configuration);
-});
-
-
-builder.Services.AddEndpointsApiExplorer();
-
+builder.AddPresentation();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.Services.Configure<RequestTimeLoggingOptions>(options => {
-    options.MaxBodyLogSize = 500;
-    options.LogSuccessResponseBody = false;
-    options.LogErrorResponseBody = true;
-    options.ExcludePaths = ["/swagger"];
-});
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
 
 var app = builder.Build();
 

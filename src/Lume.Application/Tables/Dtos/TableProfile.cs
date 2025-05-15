2c8fc689f810b40fff56765d1a2f7ae1b31f5972
Rohozin Maksym <rohozin.max@gmail.com>
2025-05-15 12:22:58 +0200

    feat: add mappings for GetAllTableQuery to support filtering and sorting options

--- src/Lume.Application/Tables/Dtos/TableProfile.cs
+++ src/Lume.Application/Tables/Dtos/TableProfile.cs
@@ -2,7 +2,9 @@ using AutoMapper;
 using Lume.Application.Tables.Commands.CreateTable;
 using Lume.Application.Tables.Commands.UpdateTable;
 using Lume.Application.Tables.Commands.DeleteTable;
+using Lume.Application.Tables.Queries.GetAllTables;
 using Lume.Domain.Entities;
+using Lume.Domain.Repositories;
 
 namespace Lume.Application.Tables.Dtos;
 
@@ -10,8 +12,15 @@ public class TablesProfile : Profile
 {
     public TablesProfile()
     {
+        // Entity to DTO mappings
         CreateMap<Table, TablesDto>();
+        
+        // Command to entity mappings
         CreateMap<CreateTableCommand, Table>();
         CreateMap<UpdateTableCommand, Table>();
+        
+        // Query to filter/sort options mappings
+        CreateMap<GetAllTableQuery, TableFilterOptions>();
+        CreateMap<GetAllTableQuery, TableSortOptions>();
     }
 }

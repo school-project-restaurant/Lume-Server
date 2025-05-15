7c2a2da78df4280b690281c80bfd089b7e213557
Rohozin Maksym <rohozin.max@gmail.com>
2025-05-15 12:22:52 +0200

    feat: add pagination, filtering, and sorting support to GetAllTablesQuery and handler

--- src/Lume.Application/Tables/Queries/GetAllTables/GetAllTablesQuery.cs
+++ src/Lume.Application/Tables/Queries/GetAllTables/GetAllTablesQuery.cs
@@ -1,9 +1,19 @@
+using Lume.Application.Common;
 using Lume.Application.Tables.Dtos;
+using Lume.Domain.Constants;
 using MediatR;
 
 namespace Lume.Application.Tables.Queries.GetAllTables;
 
-public class GetAllTableQuery : IRequest<IEnumerable<TablesDto>>
+public class GetAllTableQuery : IRequest<PagedResult<TablesDto>>
 {
-
+    public int? SearchNumber { get; set; }
+    public int? SearchMinSeats { get; set; }
+    public int? SearchMaxSeats { get; set; }
+    
+    public int PageSize { get; set; } = 10; // Default page size
+    public int PageIndex { get; set; } = 1; // Default page index
+    
+    public string? SortBy { get; set; }
+    public SortDirection? SortDirection { get; set; }
 }

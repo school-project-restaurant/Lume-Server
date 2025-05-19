53ca36dabb525059a4cc25e8bba10e89c1edc585
Rohozin Maksym <rohozin.max@gmail.com>
2025-05-19 10:41:48 +0200

    refactor: simplify GetDishById, DeleteDish, and UpdateDish methods by removing NotFound handling

--- src/Lume.API/Controllers/DishesController.cs
+++ src/Lume.API/Controllers/DishesController.cs
@@ -26,14 +26,8 @@ public class DishesController(IMediator mediator) : ControllerBase
     [AllowAnonymous]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> GetDishById([FromRoute] Guid id)
-    {
-        var plate = await mediator.Send(new GetDishByIdQuery(id));
-        if (plate is null)
-            return NotFound($"Dish with ID {id} not found");
-
-        return Ok(plate);
-    }
+    public async Task<IActionResult> GetDishById([FromRoute] Guid id) =>
+        Ok(await mediator.Send(new GetDishByIdQuery(id)));
 
     [HttpPost]
     [ProducesResponseType(StatusCodes.Status201Created)]
@@ -50,11 +44,8 @@ public class DishesController(IMediator mediator) : ControllerBase
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> DeleteDish([FromRoute] Guid id)
     {
-        var isDeleted = await mediator.Send(new DeleteDishCommand(id));
-        if (isDeleted)
-            return NoContent();
-
-        return NotFound($"Dish with ID {id} not found");
+        await mediator.Send(new DeleteDishCommand(id));
+        return NoContent();
     }
     
     [HttpPatch("{id:guid}")]
@@ -64,10 +55,7 @@ public class DishesController(IMediator mediator) : ControllerBase
     public async Task<IActionResult> UpdateDish([FromRoute] Guid id, [FromBody] UpdateDishCommand command)
     {
         command.Id = id;
-        var isUpdated = await mediator.Send(command);
-        if (isUpdated)
-            return NoContent();
-
-        return NotFound($"Plate with ID {id} not found");
+        await mediator.Send(command);
+        return NoContent();
     }
 }
